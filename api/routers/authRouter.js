4951c310d7c433676e0590651c41a5af4835c2e1
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-02-04 15:37:34 -0600

    procfile

---
+++ api/routers/authRouter.js
@@ -0,0 +1,24 @@
+// const express = require('express');
+// const router = express.Router();
+
+
+// module.exports = router;
+
+// const db = require('../../data/helpers/schoolModel.js');
+
+// router.route('/login').post(login);
+
+
+// router.route('/register').post(register);
+
+//     function get(req, res) {
+//         const id = req.params.id;
+//         db.get(id)
+//             .then(result => {
+//                 res.status(200).json(result);
+//             })
+//             .catch(err => res.status(500).json({ msg: 'name must be unique', err }));
+//     }
+
+
+
